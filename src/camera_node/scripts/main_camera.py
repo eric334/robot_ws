f0a04fe44b70ff11bbb8e75886a4dad38bcee45d
eric-buck <53630697+eric-buck@users.noreply.github.com>
2022-02-27 13:56:06 -0800

    start cam compression

--- src/camera_node/scripts/main_camera.py
+++ src/camera_node/scripts/main_camera.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 import rospy
 from geometry_msgs.msg import Twist
-from usb_cam.
 
-class cameraClass:
+class Node:
 
     def __init__(self):
         
--- src/nordic_node/scripts/nordic_send.py
+++ src/nordic_node/scripts/nordic_send.py
@@ -2,25 +2,29 @@
 import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
+from sensor_msgs.msg import CompressedImage
 from serial import Serial, serialutil
 
-class nordicSendClass:
+class Node:
 
     def __init__(self):
         
-        self._sub = rospy.Subscriber('main_nordic_send', Joy, self._callback)
-        self._pub = rospy.Publisher('main_nordic_recv', Twist)
+        dev = rospy.get_param("~dev", "/dev/ttyACM0")
+        baud = int(rospy.get_param("~baud", "115200"))
+        
+        self.serial = Serial(dev, timeout=1, baudrate=baud)
+
+        self._sub = rospy.Subscriber('send_data_camera', CompressedImage, self.callback)
+        self._pub = rospy.Subscriber('send_data_map', CompressedImage, self.callback)
 
         rospy.spin()
 
-    def _callback(self, data):
-        twist = Twist()
-        twist.linear.x = data.axes[1]
-        twist.angular.z = data.axes[0]
+    def callback(self, data):
 
-        self._pub.publish(twist)
-        
+        self.serial.write(data)
+
 
 if __name__ == '__main__':
-    rospy.init_node('main_joy', anonymous=True)
-    joyclass = joyClass()
+    rospy.init_node('nordic_send', anonymous=True)
+    node = Node()
+    node.run()
