7afd2bcc558c7336d418983eda3e2867ed827110
Eric <eric334@ucsb.edu>
2022-03-03 18:51:04 +0000

    usb cam compressedimage

---
+++ build/maestro_node/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nvidia/robot_ws/src/maestro_node
+# Build directory: /home/nvidia/robot_ws/build/maestro_node
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- src/nordic_node/scripts/nordic_send.py
+++ src/nordic_node/scripts/nordic_send.py
@@ -5,17 +5,25 @@ from sensor_msgs.msg import Joy
 from sensor_msgs.msg import CompressedImage
 from serial import Serial, serialutil
 
+import dynamic_reconfigure.client
+
+camera_topic = rospy.get_param("~camera_topic", "usb_cam/image_raw/compressed")
+hector_topic = rospy.get_param("~hector_topic", "TODO")
+jpeg_quality = rospy.get_param("~jpeg_quality_level", "50")
+
 class Node:
 
     def __init__(self):
-        
+        global camera_topic
+        global hector_topic
+
         dev = rospy.get_param("~dev", "/dev/ttyACM0")
         baud = int(rospy.get_param("~baud", "115200"))
         
         self.serial = Serial(dev, timeout=1, baudrate=baud)
 
-        self._sub = rospy.Subscriber('usb_cam/compressed', CompressedImage, self.callback)
-        self._pub = rospy.Subscriber('send_data_map', CompressedImage, self.callback)
+        self.sub_camera = rospy.Subscriber(camera_topic, CompressedImage, self.callback)
+        self.sub_hector = rospy.Subscriber(hector_topic, CompressedImage, self.callback)
 
     def run(self):
         rospy.spin()
@@ -24,7 +32,21 @@ class Node:
         self.serial.write(data)
 
 
+
+def set_compressedimage_quality(topic):
+        client = dynamic_reconfigure.client.Client(topic, timeout = 3)
+        parameters = client.get_configuration()
+        params = { 'format' : 'jpeg', 'jpeg_quality' : int(jpeg_quality) }
+        config = client.update_configuration(params)
+
 if __name__ == '__main__':
     rospy.init_node('nordic_send', anonymous=True)
+
+    set_compressedimage_quality(camera_topic)
+    set_compressedimage_quality(hector_topic)
+
     node = Node()
     node.run()
+
+    
+
