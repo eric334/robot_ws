698eea32d88d12214d74c7b5f17f54df29e08389
Eric <eric334@ucsb.edu>
2022-03-08 19:29:05 +0000

    resolved issues with camera

--- src/usb_cam/src/usb_cam.cpp
+++ src/usb_cam/src/usb_cam.cpp
@@ -375,6 +375,10 @@ int UsbCam::init_mjpeg_decoder(int image_width, int image_height)
   }
 
   avcodec_context_ = avcodec_alloc_context3(avcodec_);
+
+  av_log_set_level(AV_LOG_ERROR);
+
+
 #if LIBAVCODEC_VERSION_MAJOR < 55
   avframe_camera_ = avcodec_alloc_frame();
   avframe_rgb_ = avcodec_alloc_frame();
