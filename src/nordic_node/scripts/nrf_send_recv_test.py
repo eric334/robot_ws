8382f73d8aaabb2decbe6b44612db238122a2aa2
Eric <eric334@ucsb.edu>
2022-04-01 19:50:23 +0000

    d

--- src/nordic_node/scripts/nrf_send_recv_test.py
+++ src/nordic_node/scripts/nrf_send_recv_test.py
@@ -4,6 +4,7 @@
 import sys
 import os
 import time
+from pympler.asizeof import asizeof
 from serial import Serial, serialutil
 
 dev = '/dev/ttyACM0'
@@ -15,9 +16,36 @@ serial.open()
 
 for i in range(10):
     string = "test" + str(i)
-    serial.write(string.encode())
+    print("send: " + string)
+    serial.write("0010".encode())
     bytesToRead = 0
     while bytesToRead == 0:
         bytesToRead = serial.inWaiting()
     data = serial.read(bytesToRead)
     print(data)
+
+def send_size_then_data(data):
+    size = asizeof(data)
+
+    if (size > 9999):
+        rospy.logerror("fatal error size larger than 9999 bytes")
+        return
+
+    size = str(size).zfill(4)
+
+    serial.write(size.encode())
+
+    bytesToRead = 0
+    while bytesToRead == 0:
+        bytesToRead = serial.inWaiting()
+    data = serial.read(bytesToRead)
+
+    if data == "1":
+        serial.write(data)
+    else:
+        rospy.logerror("fatal error did not recieve reply")
+        return
+
+    
+
+    
