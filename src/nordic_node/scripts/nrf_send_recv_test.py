e919eae58964c3234c3fbe59ebc3e17aa0d72610
Eric <eric334@ucsb.edu>
2022-04-08 22:26:14 +0000

    d

--- src/nordic_node/scripts/nrf_send_recv_test.py
+++ src/nordic_node/scripts/nrf_send_recv_test.py
@@ -6,6 +6,7 @@ import os
 import time
 from pympler.asizeof import asizeof
 from serial import Serial, serialutil
+import math
 
 dev = '/dev/ttyACM0'
 baud = 115200
@@ -50,12 +51,26 @@ def send_size_then_data(data):
 
     serial.write(data)
 
+def send_as_chunks(data):
+    data = bytes(data)
+    size = asizeof(data)
+    print("Size: " + str(size))
+    chunk_size = 64
+    iterations = int(math.ceil(size / chunk_size))
+    
+    for i in range(iterations):
+        chunk = data[i*64: i*64 + 63]
+        print (chunk)
+        serial.write(chunk)
+        bytesToRead = 0
+        while bytesToRead == 0:
+            bytesToRead = serial.inWaiting()
+        echo = serial.read(bytesToRead)
+        print(echo)
 
-send_size_then_data("ut I must explain to you how all this\n mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete \naccount of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?ut I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?ut I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?ut I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
-bytesToRead = 0
-while bytesToRead == 0:
-    bytesToRead = serial.inWaiting()
-echo = serial.read(bytesToRead)
-print("echo: " + echo)
+
+
+
+send_as_chunks("the quick brown fox jumped over the lazy dog")
 
     
